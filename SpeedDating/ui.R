9342b9067641ed496ec94ffa924d182c83a95086
Darwin Jones <darwinj@uw.edu>
2017-12-05 14:38:30 -0800

    organized the shiny ui

--- SpeedDating/ui.R
+++ SpeedDating/ui.R
@@ -1,33 +1,32 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
+my.ui <- fluidPage(
+    # page title
+    titlePanel("Speed Dating Experiment"),
+    
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+        conditionalPanel(condition="input.conditionedPanels==2",
+                        helpText("Isaiah")
+            
+        ),
+        conditionalPanel(condition="input.conditionedPanels==3",
+                         helpText("Zach")
+                         
+        ),
+        conditionalPanel(condition="input.conditionedPanels==4",
+                         helpText("Darwin"),
+                         p('test')
+        )
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+        tabsetPanel( type = "tabs",
+                     tabPanel("About", value = 1),
+                     tabPanel("Isaiah", value = 2),
+                     tabPanel("Zach", value = 3),
+                     tabPanel("Darwin", value = 4),
+                     id = "conditionedPanels"
+        )
     )
-  )
-))
+)
