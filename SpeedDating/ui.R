ae6b7802eaac466585d44d678328d255f29c2068
Darwin Jones <darwinj@uw.edu>
2017-12-05 23:14:10 -0800

    ui formatting

--- SpeedDating/ui.R
+++ SpeedDating/ui.R
@@ -22,7 +22,8 @@ my.ui <- fluidPage(
     
     mainPanel(
         tabsetPanel( type = "tabs",
-                     tabPanel("About", value = 1),
+                     tabPanel("About", value = 1,
+                              includeMarkdown("description.md")),
                      tabPanel("Isaiah", value = 2),
                      tabPanel("Zach", value = 3),
                      tabPanel("Darwin", value = 4),
