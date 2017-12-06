47debdbcd056ac965594399b00a86b28e3fa45cf
Zach Ryan <zj.ryan@hotmail.com>
2017-12-06 00:03:01 -0800

    radio buttons

--- SpeedDating/ui.R
+++ SpeedDating/ui.R
@@ -11,8 +11,16 @@ my.ui <- fluidPage(
             
         ),
         conditionalPanel(condition="input.conditionedPanels==3",
-                         helpText("Zach")
-                         
+                         helpText("Zach"),
+                        
+        column(12,offset=1,
+        div(style = "font-size: 30px; padding:10px;",
+        radioButtons(inputId = "Button", "What Would You Like To Find Out?", 
+                     choices = c("What Men Think Men Want", 
+                                 "What Men Think Women Want", 
+                                 "What Women Think Women Want", 
+                                 "What Women Think Men Want"))))                
+                          
         ),
         conditionalPanel(condition="input.conditionedPanels==4",
                          helpText("Darwin"),
